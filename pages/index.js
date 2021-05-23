243db1dfd2261d989f88bad395d8bfc43825416e
unknown <jorammanoah1@gmail.com>
2021-05-23 01:09:24 -0700

    Added avatar component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
-
+import Head from "next/Head";
 export default function Home() {
   return (
-    <div >
-      hahaah
+    <div>
+      <Head>
+        <title>Wells</title>
+      </Head>
+      {/* Header */}
+      <header className="flex w-full justify-between p-5 text-sm text-gray-700">
+        {/* left */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
+        {/* right */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+        </div>
+        {/* icon */}
+        {/* avatar */}
+      </header>
     </div>
-  )
+  );
 }
